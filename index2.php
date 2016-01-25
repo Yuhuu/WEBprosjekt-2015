8e45cb561b41e484284d337d5bb94ef19cf476ff
yuanxin huang <515872005@qq.com>
2016-01-25 23:10:41 +0100

    add login and logo
    add login and logo

--- index2.php
+++ index2.php
@@ -4,17 +4,56 @@
 
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<link rel="stylesheet" href="style.css" type="text/css" />
+<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
 <script src="angular-1.0.1.min.js"></script>
+<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="style.css" type="text/css"/>
+
 <title></title>
 </head>
+<script> 
+$(document).ready(function(){
+    $("#flip").click(function(){
+        $("#panel").slideToggle("slow");
+    });
+});
+</script>
 <body>
-		<h1>Database og XML,angular</h1>
+
+<nav class="navbar-custom navbar-inverse navbar-fixed-top">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+       <a class="pull-left" href="#"><img src="/WEBprosjekt-2015/images/logo.png" alt="logo View">
+      	</a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+        <ul class="nav navbar-nav navbar-right">
+          <li><a href="#section1">Chinese</a></li>
+          <li><a href="#section2">section2</a></li>
+          <li><a href="#section3">section3</a></li>
+          <li><a href="#section3">section4</a></li>
+        </ul>
+      	<ul class="nav navbar-nav navbar-right">
+        <li><a href="#" id ="flip" ><i class="fa fa-user"></i><i class="fa fa-list"></i>
+Min side</a></li>
+      	</ul>
+    </div>
+  </div>
+</nav>
+
 <?php
-echo "Motovorgn";
+include "login.php";
 ?>
-<?php
-		/*$db = new mysqli("student.cs.hioa.no", "s184519",null,"s184519");
+<!-- ?php
+		$db = new mysqli("student.cs.hioa.no", "s184519",null,"s184519");
 		$iUser = NULL;
 		$iMessage = NULL;
 		if($db->connect_errno > 0){
@@ -59,34 +98,32 @@ echo "Motovorgn";
 			mysqli_query($db, "INSERT INTO `s184519`.`Forum` (`ID`, `User`,'Title','msg','pic','time') VALUES (NULL,'$iUser','$iTitle', '$iMessage', '$iPic','NULL')");
 		}
 		echo mysqli_error($db);
-		$db->close();*/
-	
-	echo '<div id="main-content" ng-controller="PostCtrl" ng-init="getPosts()">
+		$db->close();
+	?> -->
+	<div id="main-content" ng-controller="PostCtrl" ng-init="getPosts()">
       <input class="hidden" type="checkbox" ng-model="viewEditPost" />
-      <a href="#" class="submitButton" ng-click="viewEditPost=!viewEditPost">+</a>
+      <a href="#" class="submitButton" ng-click="viewEditPost=!viewEditPost"></a>
 	<form class="postForm" ng-submit="newPost(this);viewEditPost=!viewEditPost">
 	  <input class="inputTitle" type="text" name="title" ng-model="title" placeholder="tittel" /><br />
 	  <textarea class="inputContent" name="content" ng-model="content" placeholder="innhold"></textarea><br />
 	  <input class="inputPsw" type="password" name="psw" ng-model="psw" placeholder="passord" />
-	  <button class="submitButton" type="submit">Legg til post</button>  <!-- translations -->
-	  <button class="submitButton" ng-click="$scope.viewEditPost=false">angre</button>  <!-- translations -->
+	  <button class="submitButton" type="submit">Legg til post</button> 
+	  <button class="submitButton" ng-click="$scope.viewEditPost=false">angre</button>  
 	</form>
-</form>
-      <!--</article><!-- the big divide between the header and the content with edit --->
-      <div ng-repeat="post in posts">
-	<article>
+	</div>
+    
+    <div ng-repeat="post in posts">
 	  <input class="hidden" type="checkbox" ng-model="post.edit" />
 	  <h2 ng-click="post.edit=!post.edit" ng-show="!post.edit">{{post.title}}</h2>
 	  <p ng-show="!post.edit">{{post.content}}</p>
 	  <form class="postForm" ng-show="post.edit" ng-submit="updatePost(post,this);post.edit=!post.edit">
 	    <input class="inputTitle" type="text" name="newPostTitle" ng-model="post.title" placeholder="tittel" /><br />
 	    <textarea class="inputContent" name="newPostContent" ng-model="post.content" placeholder="innhold"></textarea><br />
-	    <button class="submitButton" type="submit">Endre post</button> <!-- translations -->
-	    <button class="submitButton" ng-click="post.edit=!post.edit">Angre</button> <!-- translations -->
+	    <button class="submitButton" type="submit">Endre post</button> 
+	    <button class="submitButton" ng-click="post.edit=!post.edit">Angre</button> 
 	    <input class="inputPsw" placeholder="passord" type="password" name="something" ng-model="psw" />
 	  </form>
-	  <div>'
-?>
+	 </div>
 </body>
 </html>
 
---
+++ login.php
@@ -0,0 +1,14 @@
+ <div class="container" id ="panel" >
+ <form class="form-inline" role="form">
+    <div class="input-group">
+      <span class="input-group-addon"><i class="fa fa-envelope-o fa-fw"></i></span>
+      <input type="email" class="form-control" id="email" placeholder="Enter email">
+    </div>
+    <div class="input-group">
+      <span class="input-group-addon"><i class="fa fa-key fa-fw"></i></span>
+      <input type="password" class="form-control" id="pwd" placeholder="Password">
+    </div>
+    <button type="submit" class="btn btn-default">Submit</button>
+  </form>
+</div>
+  	 
