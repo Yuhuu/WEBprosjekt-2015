82d9b8e59e08af18e37c95fa677d7085e210c752
yuanxin huang <515872005@qq.com>
2016-01-27 00:16:15 +0100

    add some database functions
    add some database functions

--- functions.php
+++ functions.php
@@ -1,14 +1,45 @@
 
 <?php
-		$connection = new mysqli("localhost","root",null,"s184519");
-		if($connection->connect_errno)
-		{
-			die('failed to connect [' . $db->connect_error . ']');
-		}
+    $connection = new mysqli("localhost", "root", null, "s184519");
+    if ($connection->connect_errno){
+       die('failed to connect ['.$connection->connect_error.']');
+    }
+    
+    function createTable($name, $query){
+       queryMysql("create table if not exists $name($query)");
+       echo "Table '$name' created already.<br>";
+    }
+    
+    function queryMysql($query){
+       global $connection;
+       $result = $connection->query($query);
+       if(!$result) die('failed to connect ['.$connection->connect_error.']');
+       return $result;
+    }
+    
+    // avoid SQL injection
+    function sanitizeString($var){
+        global $connection;
+        $var = strip_tags($var);
+        $var = htmlentities($var);
+        $var = stripslashes($var);
+        return $connection->real_escape_string($var);
+  }
 
-		
-		$db->close();
+  function showProfile($user)
+  {
+    if (file_exists("$user.jpg"))
+      echo "<img src='$user.jpg' style='float:left;'>";
 
+    $result = queryMysql("SELECT * FROM profiles WHERE user='$user'");
+
+    if ($result->num_rows)
+    {
+      $row = $result->fetch_array(MYSQLI_ASSOC);
+      echo stripslashes($row['text']) . "<br style='clear:left;'><br>";
+    }
+  }
+    //$db->close();
 ?> 
 
 
